7ba1fd15ec9538f76fde8d89a31f8834d77451f1
ruslan <rus.barik@gmail.com>
2018-06-10 13:16:07 +0300

    Create Notification

--- app/src/main/java/com/example/android/background/MainActivity.java
+++ app/src/main/java/com/example/android/background/MainActivity.java
@@ -25,10 +25,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.background.sync.ReminderTasks;
 import com.example.android.background.sync.WaterReminderIntentService;
+import com.example.android.background.utilities.NotificationUtils;
 import com.example.android.background.utilities.PreferenceUtilities;
-
-import static com.example.android.background.sync.ReminderTasks.ACTION_INCREMENT_WATER_COUNT;
 
 public class MainActivity extends AppCompatActivity implements
         SharedPreferences.OnSharedPreferenceChangeListener {
@@ -84,12 +84,15 @@ public class MainActivity extends AppCompatActivity implements
         if (mToast != null) mToast.cancel();
         mToast = Toast.makeText(this, R.string.water_chug_toast, Toast.LENGTH_SHORT);
         mToast.show();
-        // TODO (15) Create an explicit intent for WaterReminderIntentService
+
         Intent incrementWaterCountIntent = new Intent(this, WaterReminderIntentService.class);
-        // TODO (16) Set the action of the intent to ACTION_INCREMENT_WATER_COUNT
-        incrementWaterCountIntent.setAction(ACTION_INCREMENT_WATER_COUNT);
-        // TODO (17) Call startService and pass the explicit intent you just created
+        incrementWaterCountIntent.setAction(ReminderTasks.ACTION_INCREMENT_WATER_COUNT);
         startService(incrementWaterCountIntent);
+    }
+
+    // TODO (15) Create a method called testNotification that triggers NotificationUtils' remindUserBecauseCharging
+    public void testNotification(View view) {
+        NotificationUtils.remindUserBecauseCharging(this);
     }
 
     @Override
